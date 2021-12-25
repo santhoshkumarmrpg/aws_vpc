be71bb12afc55d1756865104ac09b18ac56a4664
santhoshkumar <santhoshkumarr@quickplay.com>
2021-12-25 17:14:34 +0530

    Updated files

--- var.tf
+++ var.tf
@@ -2,12 +2,12 @@ variable "AWS_REGION" {
     default = "ap-south-1"
 }
 
-variable "AMI" {
-    type = "map"
+# variable "AMI" {
+#     type = "map"
     
-    default {
-        ap-south-1 = "ami-0902b0be1c5a22a55"
-        ap-southeast-1 = "ami-0686c754"
-    }
-}
+#     default {
+#         ap-south-1 = "ami-0902b0be1c5a22a55"
+#         ap-southeast-1 = "ami-0686c754"
+#     }
+# }
 
