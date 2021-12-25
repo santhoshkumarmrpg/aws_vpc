23b45d760d7387d8d95b7b654a5bbe6c4084c812
santhoshkumar <santhoshkumarr@quickplay.com>
2021-12-25 17:13:13 +0530

    Updated files

---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "AWS_REGION" {    
+    default = "ap-south-1"
+}
+
+variable "AMI" {
+    type = "map"
+    
+    default {
+        ap-south-1 = "ami-0902b0be1c5a22a55"
+        ap-southeast-1 = "ami-0686c754"
+    }
+}
+
